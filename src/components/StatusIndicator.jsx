935d4e9d4540f30c17962c20956d5f7e05af5ff3
chadgolden1 <chad@chadgolden.com>
2025-03-11 18:09:34 -0400

    Loading indicator, resize canvas

---
+++ src/components/StatusIndicator.jsx
@@ -0,0 +1,69 @@
+const StatusIndicator = ({ error, isLoading, pageRendered }) => {
+  let status = {
+    color: "green",
+    animation: "",
+    text: "Ready",
+    bgLight: "bg-green-100",
+    bgDark: "dark:bg-green-900",
+    textLight: "text-green-800",
+    textDark: "dark:text-green-200",
+    dotLight: "bg-lime-600",
+    dotDark: "dark:bg-lime-300"
+  };
+
+  if (error) {
+    status = {
+      color: "red",
+      animation: "",
+      text: "Error",
+      bgLight: "bg-red-100",
+      bgDark: "dark:bg-red-900",
+      textLight: "text-red-800",
+      textDark: "dark:text-red-200",
+      dotLight: "bg-red-600",
+      dotDark: "dark:bg-red-300"
+    };
+  } else if (!pageRendered) {
+    status = {
+      color: "purple",
+      animation: "animate-ping",
+      text: "Initializing: This will take a few seconds...",
+      bgLight: "bg-purple-100",
+      bgDark: "dark:bg-purple-900",
+      textLight: "text-purple-800",
+      textDark: "dark:text-purple-200",
+      dotLight: "bg-purple-600",
+      dotDark: "dark:bg-purple-200"
+    };
+  } else if (isLoading) {
+    status = {
+      color: "blue",
+      animation: "animate-ping",
+      text: "Rendering...",
+      bgLight: "bg-blue-100",
+      bgDark: "dark:bg-blue-900",
+      textLight: "text-blue-800",
+      textDark: "dark:text-blue-200",
+      dotLight: "bg-blue-600",
+      dotDark: "dark:bg-blue-300"
+    };
+  }
+
+  return (
+    <div className="mt-5 block text-center">
+      <span className="sr-only">Status: {status.text}</span>
+      <span className={`
+        inline-flex items-center
+        ${status.bgLight} ${status.bgDark}
+        ${status.textLight} ${status.textDark}
+        px-3 py-0.5
+        rounded-full`}>
+        <span className={`absolute inline-flex size-2.25 ${status.animation} rounded-full ${status.dotLight} ${status.dotDark}`}></span>
+        <span className={`relative inline-flex size-2.25 me-2.25 ${status.dotLight} ${status.dotDark} rounded-full`}></span>
+        {status.text}
+      </span>
+    </div>
+  );
+};
+
+export default StatusIndicator;
